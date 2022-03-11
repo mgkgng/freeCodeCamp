61b4b77d660ace0ed3adb2171553f0d0ba9e1d04
Minguk Gaang <mgk@MacBook-Pro-de-Minguk.local>
2022-03-11 13:25:48 +0100

    learning

--- KivyCourse/TheLab/main.py
+++ KivyCourse/TheLab/main.py
@@ -2,10 +2,21 @@ from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.uix.button import Button
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.anchorlayout import AnchorLayout
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.stacklayout import StackLayout
+
+class StackLayoutExample(StackLayout):
+	pass
+
+#class GridLayoutExample(GridLayout):
+#	pass
+
+class AnchorLayoutExample(AnchorLayout):
+	pass
 
 class BoxLayoutExample(BoxLayout):
 	pass
-
 	"""def __init__(self, **kwargs):
 		super().__init__(**kwargs)
 		self.orientation = "vertical"
